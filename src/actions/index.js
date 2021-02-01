ae033b559ec4a8fc34c17a9b3032408746fc7f10
JorgenBy <57228187+JorgenBy@users.noreply.github.com>
2021-02-01 16:25:25 +0100

    App ready
    App is completed in the course

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,17 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 import jsonplaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    //lodash chain
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+}
 
 export const fetchPosts = () => async dispatch => {
         const response = await jsonplaceholder.get('/posts');
@@ -7,5 +20,19 @@ export const fetchPosts = () => async dispatch => {
             type: 'FETCH_POSTS',
             payload: response.data
         });
-    };
+};
 
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}` );
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+
+// Lodash Memoize solution for handle problem with multiple fetching of users
+/* export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}` );
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+}); */
